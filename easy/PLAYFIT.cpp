aa2d9661f6da8ecc2b60b230b706d838c4ee7efa
Vasu Goel <goelvasu0712@gmail.com>
2020-01-26 23:45:33 +0530

    Fit to Play

---
+++ easy/PLAYFIT.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Vasu Goel on 1/26/20.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        int min, goals, max = 0;
+        cin >> min;
+        for(int i = 1; i < n; i++) {
+            cin >> goals;
+            if(goals < min)  min = goals;
+            else  if(goals - min > max)  max = goals - min;
+        }
+        if(max)  cout << max << '\n';
+        else  cout << "UNFIT\n";
+    }
+    return 0;
+}
